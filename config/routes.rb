118e08bd31c883c81ef49739fb835a17fd1d44eb
winktechnologies <oym@poczta.onet.pl>
2012-09-12 13:09:47 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
-  root :to => 'static_pages#home'
+  root to: 'static_pages#home'
 
   match '/signup', :to => 'users#new'
-
-  root :to => 'static_pages#home'
 
   match '/help', :to => 'static_pages#help'
 
