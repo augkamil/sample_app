5d8eb81e580ce1ab6ac25dda8887ce9ae1f8b741
winktechnologies <oym@poczta.onet.pl>
2012-09-12 22:59:48 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
   resources :users
-
   root to: 'static_pages#home'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/signup', :to => 'users#new'
 
